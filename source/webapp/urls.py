ec41592ff55f4439f2604d0e01f1205880e5aab8
Beknasar <680633@gmail.com>
2024-02-18 17:28:09 +0300

    Initial amend

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index_view
+from .views import index_view, article_create_view
 
 app_name = 'webapp'
 
 urlpatterns = [
     path('', index_view),
+    path('articles/add/', article_create_view),
 ]
