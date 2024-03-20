6c27c38012c693aca5d735a25abdbe7f11c09dd6
Beknazar <680633@gmail.com>
2024-03-20 01:28:08 -1200

    Добавлены валидаторы на минимальную длину -классовый и не-классовый.

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from django.core.validators import BaseValidator
+from django.utils.deconstruct import deconstructible
 from .models import STATUS_CHOICES, Article, Tag
 
 
@@ -7,8 +9,25 @@ BROWSER_DATETIME_FORMAT = "%Y-%m-%dT%H:%M"
 default_status = STATUS_CHOICES[0][0]
 
 
+def at_least_10(string):
+    if len(string) < 10:
+        raise ValidationError('Заголовок слишком короткий!')
+
+
+@deconstructible
+class MinLengthValidator(BaseValidator):
+    message = 'Value "%(value)s" has length of %(show_value)d! It should be at least %(limit_value)d symbols long!'
+    code = 'too_short'
+
+    def compare(self, value, limit):
+        return value < limit
+
+    def clean(self, value):
+        return len(value)
+
+
 class ArticleForm(forms.Form):
-    title = forms.CharField(max_length=200, required=True, label='Заголовок')
+    title = forms.CharField(max_length=200, required=True, label='Заголовок', validators=[MinLengthValidator(10)])
     text = forms.CharField(max_length=3000, required=True, label='Текст', widget=forms.Textarea)
     author = forms.CharField(max_length=40, required=True, initial='Unknown', label='Автор')
     status = forms.ChoiceField(choices=STATUS_CHOICES, initial=default_status, label='Модерация')
@@ -18,12 +37,6 @@ class ArticleForm(forms.Form):
                                                     '%Y-%m-%d %H:%M:%S'],
                                      widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}))
     tags = forms.ModelMultipleChoiceField(required=False, label='Теги', queryset=Tag.objects.all())
-
-    def clean_title(self):
-        title = self.cleaned_data['title']
-        if len(title) < 10:
-            raise ValidationError('Заголовок слишком короткий!')
-        return title
 
     def clean(self):
         cleaned_data = super().clean()
