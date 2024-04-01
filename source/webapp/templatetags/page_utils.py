5490c19d03dc9a126b7200b75da4c01f2c1c60c4
Beknazar <680633@gmail.com>
2024-04-01 00:16:52 -1200

    С главной страницы убран метод get_context_data, т.к. больше нет необходимости передавать search в контексте.

--- source/webapp/templatetags/page_utils.py
+++ source/webapp/templatetags/page_utils.py
@@ -7,6 +7,5 @@ register = template.Library()
 @register.filter
 def page_query_string(request, page_number):
     query_args = request.GET.copy()
-    print(query_args)
     query_args['page'] = page_number
     return query_args.urlencode()
